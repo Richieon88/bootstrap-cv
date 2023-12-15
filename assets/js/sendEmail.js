a59da426cef1285a83a1c8bb5db9199db21eae95
Richieon88 <>
2023-12-15 00:11:52 +0000

    Implement email.js integration on the contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendEmail(contactForm) {
+    email.js.send("service_alkte4b", "template_jm7c7dw", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;
+}
